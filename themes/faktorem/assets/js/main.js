4a9715b276233b618a549c80991b9629b5862c8c
Antonija Pek <antonijapek@gmail.com>
2019-01-19 13:31:23 +0200

    Site done

--- themes/faktorem/assets/js/main.js
+++ themes/faktorem/assets/js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
-  $('.owl-carousel').owlCarousel();
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    dots: true,
+    margin: 16,
+    animateOut: 'fadeOut',
+    center: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      }
+    }
+  });
 });
